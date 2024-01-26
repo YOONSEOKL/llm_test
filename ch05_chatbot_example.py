52a8ae0db1d09181904ca52895bad1c7a6aade5e
YOONSEOKL <154576492+YOONSEOKL@users.noreply.github.com>
2024-01-26 11:16:33 +0900

    Add files via upload

--- ch05_chatbot_example.py
+++ ch05_chatbot_example.py
@@ -9,7 +9,7 @@ from openai.embeddings_utils import get_embedding
 import streamlit as st
 from streamlit_chat import message
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 #folder_path와 folder_name을 결합하여 file_path = ./data/embedding.csv'
 folder_path = './data'
